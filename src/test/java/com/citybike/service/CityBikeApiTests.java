b7ce86f3d0cbdbdb08ffd6c961dd0bfd4b5e0691
MaryaManju Induri <marya.induri@gmail.com>
2020-11-17 10:09:57 +0000

    Rest assured test cases for city bike (sample)

--- src/test/java/com/citybike/service/CityBikeApiTests.java
+++ src/test/java/com/citybike/service/CityBikeApiTests.java
@@ -1,25 +1,31 @@
 package com.citybike.service;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class CityBikeApiTests {
 
     private final String host = "http://api.citybik.es/v2/networks";
 
+    @Ignore
     @Test
     public void testNetworks() {
 
-        given().get(host)
+        given().get("http://api.citybik.es/v2/networks")
                 .then()
-                .log().all()
                 .assertThat()
-                .statusCode(200);
+                .statusCode(200)
+                .and()
+                .log().all()
+                .body("networks[0].location.city", notNullValue());
 
     }
 
+    @Ignore
     @Test
     public void testLatLong() {
 
@@ -31,4 +37,34 @@ public class CityBikeApiTests {
                 .body("network.location.longitude", notNullValue());
     }
 
+    @Ignore
+    @Test
+    public void testCountry() {
+
+        given().get(host + "/bycyklen")
+                .then()
+                .log().ifValidationFails()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testCity() {
+
+        given().get(host + "/bycyklen")
+                .then()
+                .log().ifValidationFails()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("network.location.city", notNullValue())
+                .and()
+                .body("network.location.country", equalTo("DK"))
+                .and()
+                .body("network.location.latitude", notNullValue())
+                .and()
+                .body("network.location.longitude", notNullValue());
+    }
+
 }
+
